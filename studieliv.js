02e987e2981940a55eb07fe5ff3b399844726455
Semicolon0 <nikolaihansen05@gmail.com>
2026-05-29 15:24:44 +0200

    Add image slider and student projects section
    Introduce a simple image carousel on studieliv: HTML updated to wrap the card image with prev/next buttons and an img#slider_billede; CSS adds layout and button styles for the slider; JS implements next/previous cycling through a small image array and updates the displayed image. Also fix a minor punctuation typo in studieliv.html and add a placeholder student projects container (#studerende_projekter / #student_project_card) to tidligere.html for future content.

--- studieliv.js
+++ studieliv.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+const billeder = [
+    "Images/placeholder.png",
+    "Images/billede2.png",
+    "Images/billede3.png"
+];
+
+let nuvaerendeBillede = 0;
+
+const sliderBillede = document.getElementById("slider_billede");
+
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+nextBtn.addEventListener("click", function () {
+
+    nuvaerendeBillede++;
+
+    if (nuvaerendeBillede >= billeder.length) {
+        nuvaerendeBillede = 0;
+    }
+
+    sliderBillede.src = billeder[nuvaerendeBillede];
+
+});
+
+prevBtn.addEventListener("click", function () {
+
+    nuvaerendeBillede--;
+
+    if (nuvaerendeBillede < 0) {
+        nuvaerendeBillede = billeder.length - 1;
+    }
+
+    sliderBillede.src = billeder[nuvaerendeBillede];
+
+});
